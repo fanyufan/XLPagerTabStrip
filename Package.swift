36ada22b252af7cc6058ad8080d0e97596c8dafd
Elshad Yarmetov <yelshad@gmail.com>
2020-10-01 11:03:03 +0400

    SwiftPM changes

--- Package.swift
+++ Package.swift
@@ -1,17 +1,31 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
-  name: "XLPagerTabStrip",
-  products: [
-    .library(
-      name: "XLPagerTabStrip",
-      targets: ["XLPagerTabStrip"])
-  ],
-  targets: [
-    .target(
-      name: "XLPagerTabStrip",
-      path: "Sources")
-  ]
+    name: "XLPagerTabStrip",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "XLPagerTabStrip",
+            targets: ["XLPagerTabStrip"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "XLPagerTabStrip",
+            path: "Sources",
+            exclude: [
+                "FXPageControl.h",
+                "FXPageControl.m"
+            ]
+        ),
+        .testTarget(
+            name: "XLPagerTabStripTests",
+            dependencies: ["XLPagerTabStrip"],
+            path: "Tests"
+        )
+    ]
 )
